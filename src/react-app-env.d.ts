4d2719f125aa676f2e3c240744d50934ef538d6b
leey0818 <leey0818@gmail.com>
2022-03-29 10:12:28 +0900

    타입 오류 수정

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,14 +1,14 @@
 /// <reference types="react-scripts" />
-
 import { IpcRenderer, IpcRendererEvent } from 'electron';
 import { IPCPageEventData, PreviewImageResult, GenerateCommandResult, ProgressHandlerData } from "./electron/types";
 
 declare global {
   interface Window {
     electron: {
-      previewImage: (data: IPCPageEventData) => Promise<PreviewImageResult>,
-      generatePDF: (data: IPCPageEventData) => Promise<GenerateCommandResult>,
-      onProgress: (callback: (event: IpcRendererEvent, data: ProgressHandlerData) => void) => IpcRenderer
+      previewImage: (data: IPCPageEventData) => Promise<PreviewImageResult>;
+      generatePDF: (data: IPCPageEventData) => Promise<GenerateCommandResult>;
+      cancelRequest: () => Promise<void>;
+      onProgressGenerate: (callback: (event: IpcRendererEvent, data: ProgressHandlerData) => void) => IpcRenderer;
     }
   }
 }
